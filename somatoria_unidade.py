fedb858e9e0932f4f32ab8ea57d3797fa5b72eaa
Rodrigo Nicola <rnicola@darkstar.example.net>
2015-10-02 10:58:50 -0300

    Caracter fmt errado

--- somatoria_unidade.py
+++ somatoria_unidade.py
@@ -7,15 +7,15 @@ def SomarAgora1(valor1, valor2):
 
     if (Resultado / 10) < 10 and (Resultado / 10) >= 1:
 
-        print 'O valor digitado é uma dezena'
+        print 'O valor digitado e uma dezena'
     elif (Resultado / 100) < 10 and (Resultado / 100) >= 1:
-        print 'O valor digitado é uma centena'
+        print 'O valor digitado e uma centena'
     elif (Resultado / 1000) < 10 and (Resultado / 1000) >= 1:
-        print 'O valor digitado é uma milhar'
+        print 'O valor digitado e uma milhar'
     elif (Resultado / 10000) < 10 and (Resultado / 10000) >= 1:
-        print 'O valor digitado é uma dezena de milhar'
+        print 'O valor digitado e uma dezena de milhar'
     else:
-        print 'O valor digitado é uma unidade'
+        print 'O valor digitado e uma unidade'
 
 
 SomarAgora1(2, 10)
